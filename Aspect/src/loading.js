e309956cb04753ebb5d63314bf7e359ac104e0c1
unknown <zhangjiangwei@douyole.com>
2016-05-26 15:03:40 +0800

    updata

--- Aspect/src/loading.js
+++ Aspect/src/loading.js
@@ -4,5 +4,10 @@ zz.loading=function(){
 	Bg.graphics.beginFill('#ffff00',.5);
 	Bg.graphics.drawRect(0,0,this.stage.width,this.stage.height);
 	this.txt = '游戏正在加载中……';
+	var text = new createjs.Text(this.txt,"20px Arial","#ff7700");	
+	text.x = (this.stage.width-text.width)/2;
+	text.y=(this.stage.height-text.height)/2;
 	this.loadBg = Bg;
+	this.loadBg.addChild(text);
+	this.stage.addChild(this.loadBg);
 }
